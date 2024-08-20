9201a71f6af3548d8df50e1b94f76b22a68d1a69
Poornima-164 <poornisundar164@gmail.com>
2024-08-20 23:19:36 +0530

    Add files via upload

---
+++ week1/day1/Firefox.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		float c=100.2f;
+		String browserName="firefox";
+		boolean isVisible=true;
+		int releaseYear=1998;
+		char browserLogo='f';
+		System.out.println ("Browser version :"+c +"\n"+w"Name of the browser :"+browserName);
+		System.out.println ("Browser is :"+isVisible + "\n"+"Year of release:"+releaseYear + "\n"+"Logo:"+browserLogo);
+	}
+
+}
---
+++ week1/day1/IsPrimeNumber.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class IsPrimeNumber {
+
+	public static void main(String[] args) {
+ int  num=7;
+ for (int i=0;i<=num;i++) {
+	 if(num%2==0)
+	 {
+		 System.out.println("Not a prime number");
+	 }else {
+		 System.out.println("is a prime number");
+	 }
+ }
+	}
+
+}
