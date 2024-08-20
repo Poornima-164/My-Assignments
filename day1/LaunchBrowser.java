836ad0e8157a88a5713eab86095b3e4c48861d2f
Poornima-164 <poornisundar164@gmail.com>
2024-08-20 23:15:14 +0530

    Add files via upload

---
+++ day1/LaunchBrowser.java
@@ -0,0 +1,14 @@
+package week2.day1;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchBrowser {
+
+	public static void main(String[] args) {
+   ChromeDriver driver=new ChromeDriver();
+   driver.manage().window().maximize();
+   driver.get("https://wwww.facebook.com");
+   driver.close();
+	}
+
+}
